1906d745129de73457a95fad87a490e5f80feab4
junyong <junyong008@gmail.com>
2024-09-17 13:48:42 +0900

    feat(app): 상단바와 네비게이션바 색상 변경

--- app/src/main/java/com/yjy/challengetogether/MainActivity.kt
+++ app/src/main/java/com/yjy/challengetogether/MainActivity.kt
@@ -2,8 +2,11 @@ package com.yjy.challengetogether
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.DisposableEffect
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.yjy.challengetogether.ui.ChallengeTogetherApp
 import com.yjy.core.designsystem.theme.ChallengeTogetherTheme
@@ -18,6 +21,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             val isDarkTheme = isSystemInDarkTheme()
 
+            DisposableEffect(isDarkTheme) {
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.auto(
+                        LIGHT_STATUS_BAR_COLOR,
+                        DARK_STATUS_BAR_COLOR,
+                    ) { isDarkTheme },
+                    navigationBarStyle = SystemBarStyle.auto(
+                        LIGHT_NAVIGATION_BAR_COLOR,
+                        DARK_NAVIGATION_BAR_COLOR,
+                    ) { isDarkTheme },
+                )
+                onDispose {}
+            }
+
             ChallengeTogetherTheme(
                 darkTheme = isDarkTheme,
             ) {
@@ -26,3 +43,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+private const val LIGHT_STATUS_BAR_COLOR = android.graphics.Color.TRANSPARENT
+private const val DARK_STATUS_BAR_COLOR = android.graphics.Color.TRANSPARENT
+private val LIGHT_NAVIGATION_BAR_COLOR = android.graphics.Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
+private val DARK_NAVIGATION_BAR_COLOR = android.graphics.Color.argb(0x80, 0x1b, 0x1b, 0x1b)
