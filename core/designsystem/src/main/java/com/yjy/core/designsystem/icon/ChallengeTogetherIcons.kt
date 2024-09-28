35058c7d9952503b81ac36163588f226a836f15d
junyong <junyong008@gmail.com>
2024-09-28 17:34:46 +0900

    style: 아이콘 디자인 변경

--- core/designsystem/src/main/java/com/yjy/core/designsystem/icon/ChallengeTogetherIcons.kt
+++ core/designsystem/src/main/java/com/yjy/core/designsystem/icon/ChallengeTogetherIcons.kt
@@ -5,7 +5,7 @@ import com.yjy.core.designsystem.R
 object ChallengeTogetherIcons {
     val Cancel = R.drawable.ic_cancel
     val Lock = R.drawable.ic_lock
-    val Mail = R.drawable.ic_mail
+    val Email = R.drawable.ic_email
     val Visibility = R.drawable.ic_visibility
     val VisibilityOff = R.drawable.ic_visibility_off
 }
