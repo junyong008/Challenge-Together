b3074cdc1fc42d61a500d7b3c329cd7cabb73550
junyong <junyong008@gmail.com>
2024-09-29 18:49:30 +0900

    style: 코틀린 가이드 미준수 스타일 수정

--- core/data/src/main/java/com/yjy/core/data/repository/AuthRepositoryImpl.kt
+++ core/data/src/main/java/com/yjy/core/data/repository/AuthRepositoryImpl.kt
@@ -3,8 +3,7 @@ package com.yjy.core.data.repository
 import com.yjy.core.common.network.NetworkResult
 import javax.inject.Inject
 
-internal class AuthRepositoryImpl @Inject constructor(
-) : AuthRepository {
+internal class AuthRepositoryImpl @Inject constructor() : AuthRepository {
 
     override suspend fun login(email: String, password: String): NetworkResult<Unit> {
         TODO("Not yet implemented")
