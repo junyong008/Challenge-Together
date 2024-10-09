6b6c0b6221932402257bafb84a5e1f69034f9b5f
junyong <junyong008@gmail.com>
2024-10-09 15:04:53 +0900

    test(feature.login): 단위 테스트 추가

--- feature/login/src/test/java/com/yjy/feature/login/LoginViewModelTest.kt
+++ feature/login/src/test/java/com/yjy/feature/login/LoginViewModelTest.kt
@@ -1,5 +1,7 @@
 package com.yjy.feature.login
 
+import com.yjy.core.common.constants.AuthConst.MAX_EMAIL_LENGTH
+import com.yjy.core.common.constants.AuthConst.MAX_PASSWORD_LENGTH
 import com.yjy.core.common.network.HttpStatusCodes.UNAUTHORIZED
 import com.yjy.core.common.network.NetworkResult
 import com.yjy.core.data.repository.AuthRepository
@@ -188,6 +190,76 @@ class LoginViewModelTest {
         assertEquals(
             expected = false,
             actual = updatedState.canTryLogin,
+        )
+    }
+
+    @Test
+    fun `login with network error should trigger NetworkError event`() = runTest {
+        // Given
+        val email = "user@example.com"
+        val password = "ValidPassword123"
+        val throwable = Throwable("Network error")
+        coEvery { authRepository.emailLogin(email, password) } returns NetworkResult.Failure.NetworkError(throwable)
+
+        // When
+        val action = LoginUiAction.OnEmailLoginClick(email, password)
+        viewModel.processAction(action)
+
+        // Then
+        assertEquals(
+            expected = LoginUiEvent.LoginFailure.NetworkError,
+            actual = viewModel.uiEvent.first(),
+        )
+    }
+
+    @Test
+    fun `password update should not update when password exceeds max length`() = runTest {
+        // Given
+        val longPassword = "a".repeat(MAX_PASSWORD_LENGTH + 1)
+
+        // When
+        viewModel.processAction(LoginUiAction.OnPasswordUpdated(longPassword))
+
+        // Then
+        val updatedState = viewModel.uiState.first()
+        assertEquals(
+            expected = "",
+            actual = updatedState.password,
+        )
+    }
+
+    @Test
+    fun `email update should not update when email exceeds max length`() = runTest {
+        // Given
+        val longEmail = "a".repeat(MAX_EMAIL_LENGTH + 1) + "@example.com"
+
+        // When
+        viewModel.processAction(LoginUiAction.OnEmailUpdated(longEmail))
+
+        // Then
+        val updatedState = viewModel.uiState.first()
+        assertEquals(
+            expected = "",
+            actual = updatedState.email,
+        )
+    }
+
+    @Test
+    fun `login with unknown error should trigger UnknownError event`() = runTest {
+        // Given
+        val email = "user@example.com"
+        val password = "ValidPassword123"
+        val throwable = Throwable("Unknown error")
+        coEvery { authRepository.emailLogin(email, password) } returns NetworkResult.Failure.UnknownApiError(throwable)
+
+        // When
+        val action = LoginUiAction.OnEmailLoginClick(email, password)
+        viewModel.processAction(action)
+
+        // Then
+        assertEquals(
+            expected = LoginUiEvent.LoginFailure.UnknownError,
+            actual = viewModel.uiEvent.first(),
         )
     }
 }
