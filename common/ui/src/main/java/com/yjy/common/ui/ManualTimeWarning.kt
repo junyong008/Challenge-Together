6ddf850a2f553bac0d7a9a12dafd762413127da0
junyong <junyong008@gmail.com>
2024-11-06 19:10:21 +0900

    refactor(common.ui): 시간 수동 설정 경고 디자인 수정

--- common/ui/src/main/java/com/yjy/common/ui/ManualTimeWarning.kt
+++ common/ui/src/main/java/com/yjy/common/ui/ManualTimeWarning.kt
@@ -2,7 +2,11 @@ package com.yjy.common.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,10 +14,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.yjy.common.designsystem.ComponentPreviews
+import com.yjy.common.designsystem.icon.ChallengeTogetherIcons
 import com.yjy.common.designsystem.theme.ChallengeTogetherTheme
 import com.yjy.common.designsystem.theme.CustomColorProvider
 
@@ -23,17 +29,26 @@ fun ManualTimeWarning(
 ) {
     Box(
         modifier = modifier
-            .clip(MaterialTheme.shapes.extraLarge)
+            .clip(MaterialTheme.shapes.large)
             .background(CustomColorProvider.colorScheme.red.copy(alpha = 0.8f))
-            .padding(horizontal = 16.dp, vertical = 8.dp),
+            .padding(horizontal = 16.dp, vertical = 12.dp),
         contentAlignment = Alignment.Center,
     ) {
-        Text(
-            text = stringResource(id = R.string.common_ui_manual_time_warning),
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.labelMedium,
-            color = Color.Black,
-        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                painter = painterResource(id = ChallengeTogetherIcons.Timer),
+                contentDescription = stringResource(id = R.string.common_ui_manual_time_warning_icon),
+                tint = Color.Black,
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = stringResource(id = R.string.common_ui_manual_time_warning),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Black,
+            )
+        }
+
     }
 }
 
