b4e144081e59b8cbdbee86197a66b9c861c4a3b5
junyong <junyong008@gmail.com>
2024-11-08 17:19:09 +0900

    feat(domain): 시작된 챌린지 조회시 최대 기록이 목표일을 넘지 않도록 제한

--- domain/src/main/java/com/yjy/domain/GetStartedChallengesUseCase.kt
+++ domain/src/main/java/com/yjy/domain/GetStartedChallengesUseCase.kt
@@ -1,8 +1,10 @@
 package com.yjy.domain
 
+import com.yjy.common.core.constants.TimeConst.SECONDS_PER_DAY
 import com.yjy.data.challenge.api.ChallengeRepository
 import com.yjy.data.user.api.UserRepository
 import com.yjy.model.challenge.SimpleStartedChallenge
+import com.yjy.model.challenge.core.TargetDays
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 import javax.inject.Inject
@@ -15,7 +17,11 @@ class GetStartedChallengesUseCase @Inject constructor(
         challengeRepository.startedChallenges,
         userRepository.timeDiff,
     ) { challenges, timeDiff ->
-        challenges.map { it.applyTimeDiff(timeDiff) }
+        challenges.map { challenge ->
+            challenge
+                .applyTimeDiff(timeDiff)
+                .applyTargetDaysLimit()
+        }
     }
 
     private fun SimpleStartedChallenge.applyTimeDiff(timeDiff: Long): SimpleStartedChallenge {
@@ -23,5 +29,15 @@ class GetStartedChallengesUseCase @Inject constructor(
             recentResetDateTime = recentResetDateTime.plusSeconds(timeDiff),
             currentRecordInSeconds = currentRecordInSeconds.minus(timeDiff),
         )
+    }
+
+    private fun SimpleStartedChallenge.applyTargetDaysLimit(): SimpleStartedChallenge {
+        val limitedRecord = when (val targetDays = targetDays) {
+            is TargetDays.Fixed -> currentRecordInSeconds.coerceAtMost(
+                targetDays.days * SECONDS_PER_DAY
+            )
+            TargetDays.Infinite -> currentRecordInSeconds
+        }
+        return copy(currentRecordInSeconds = limitedRecord)
     }
 }
