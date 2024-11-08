d8d110fa441788c7a1e49eb910299ddfa603b70b
junyong <junyong008@gmail.com>
2024-11-08 17:17:52 +0900

    refactor: 카테고리 목록 및 목표일 ui 모듈로 이동

--- feature/addchallenge/src/main/java/com/yjy/feature/addchallenge/SetCategoryScreen.kt
+++ feature/addchallenge/src/main/java/com/yjy/feature/addchallenge/SetCategoryScreen.kt
@@ -1,34 +1,14 @@
 package com.yjy.feature.addchallenge
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -36,14 +16,11 @@ import com.yjy.common.designsystem.ThemePreviews
 import com.yjy.common.designsystem.component.ChallengeTogetherBackground
 import com.yjy.common.designsystem.component.ChallengeTogetherBottomAppBar
 import com.yjy.common.designsystem.component.TitleWithDescription
-import com.yjy.common.designsystem.extensions.getDisplayNameResId
-import com.yjy.common.designsystem.extensions.getIconResId
 import com.yjy.common.designsystem.theme.ChallengeTogetherTheme
 import com.yjy.common.designsystem.theme.CustomColorProvider
+import com.yjy.common.ui.CategoryList
 import com.yjy.feature.addchallenge.model.AddChallengeUiAction
 import com.yjy.feature.addchallenge.model.AddChallengeUiState
-import com.yjy.feature.addchallenge.navigation.AddChallengeStrings
-import com.yjy.model.challenge.core.Category
 
 @Composable
 internal fun SetCategoryRoute(
@@ -89,8 +66,8 @@ internal fun SetCategoryScreen(
         ) {
             Spacer(modifier = Modifier.height(80.dp))
             TitleWithDescription(
-                titleRes = AddChallengeStrings.feature_addchallenge_title_set_category,
-                descriptionRes = AddChallengeStrings.feature_addchallenge_description_set_category,
+                titleRes = R.string.feature_addchallenge_title_set_category,
+                descriptionRes = R.string.feature_addchallenge_description_set_category,
             )
             Spacer(modifier = Modifier.height(50.dp))
             CategoryList(
@@ -106,102 +83,6 @@ internal fun SetCategoryScreen(
                 modifier = Modifier.weight(1f),
             )
         }
-    }
-}
-
-@Composable
-private fun CategoryList(
-    selectedCategory: Category,
-    onCategorySelected: (Category, String) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = modifier,
-    ) {
-        items(Category.entries) { category ->
-            val categoryTitle = stringResource(
-                id = if (category == Category.ALL) {
-                    AddChallengeStrings.feature_addchallenge_my_challenge
-                } else {
-                    category.getDisplayNameResId()
-                },
-            )
-
-            CategoryItem(
-                category = category,
-                categoryTitle = categoryTitle,
-                isSelected = category == selectedCategory,
-                onClick = {
-                    onCategorySelected(category, categoryTitle)
-                },
-            )
-        }
-    }
-}
-
-@Composable
-private fun CategoryItem(
-    category: Category,
-    categoryTitle: String,
-    isSelected: Boolean,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    val shape = MaterialTheme.shapes.medium
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .clip(shape)
-            .background(CustomColorProvider.colorScheme.surface)
-            .then(
-                if (isSelected) {
-                    Modifier.border(
-                        width = 2.dp,
-                        color = CustomColorProvider.colorScheme.brand,
-                        shape = shape,
-                    )
-                } else {
-                    Modifier
-                },
-            )
-            .clickable(
-                role = Role.RadioButton,
-                onClick = onClick,
-            ),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Box(
-            modifier = Modifier
-                .padding(16.dp)
-                .clip(MaterialTheme.shapes.extraLarge)
-                .background(CustomColorProvider.colorScheme.background),
-        ) {
-            Icon(
-                painter = painterResource(id = category.getIconResId()),
-                contentDescription = categoryTitle,
-                tint = CustomColorProvider.colorScheme.onBackgroundMuted,
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .padding(8.dp),
-            )
-        }
-        Text(
-            text = categoryTitle,
-            color = CustomColorProvider.colorScheme.onSurface,
-            style = MaterialTheme.typography.labelMedium,
-            modifier = Modifier.weight(1f),
-        )
-        RadioButton(
-            selected = isSelected,
-            onClick = null,
-            modifier = Modifier.padding(end = 16.dp),
-            colors = RadioButtonDefaults.colors(
-                selectedColor = CustomColorProvider.colorScheme.brand,
-                unselectedColor = CustomColorProvider.colorScheme.onSurfaceMuted,
-            ),
-        )
     }
 }
 
--- feature/addchallenge/src/main/java/com/yjy/feature/addchallenge/SetTargetDayScreen.kt
+++ feature/addchallenge/src/main/java/com/yjy/feature/addchallenge/SetTargetDayScreen.kt
@@ -1,61 +1,27 @@
 package com.yjy.feature.addchallenge
 
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.yjy.common.core.constants.ChallengeConst.INIT_CHALLENGE_TARGET_DAYS
-import com.yjy.common.core.constants.ChallengeConst.MAX_CHALLENGE_TARGET_DAYS
-import com.yjy.common.core.constants.ChallengeConst.MIN_CHALLENGE_TARGET_DAYS
 import com.yjy.common.designsystem.ThemePreviews
 import com.yjy.common.designsystem.component.ChallengeTogetherBackground
 import com.yjy.common.designsystem.component.ChallengeTogetherBottomAppBar
-import com.yjy.common.designsystem.component.CursorLessNumberTextField
 import com.yjy.common.designsystem.component.TitleWithDescription
-import com.yjy.common.designsystem.icon.ChallengeTogetherIcons
 import com.yjy.common.designsystem.theme.ChallengeTogetherTheme
 import com.yjy.common.designsystem.theme.CustomColorProvider
+import com.yjy.common.ui.TargetDay
 import com.yjy.feature.addchallenge.model.AddChallengeUiAction
 import com.yjy.feature.addchallenge.model.AddChallengeUiState
-import com.yjy.feature.addchallenge.navigation.AddChallengeStrings
-import com.yjy.model.challenge.core.TargetDays
 
 @Composable
 internal fun SetTargetDayRoute(
@@ -101,191 +67,15 @@ internal fun SetTargetDayScreen(
         ) {
             Spacer(modifier = Modifier.height(80.dp))
             TitleWithDescription(
-                titleRes = AddChallengeStrings.feature_addchallenge_title_set_target_day,
-                descriptionRes = AddChallengeStrings.feature_addchallenge_description_set_target_day,
+                titleRes = R.string.feature_addchallenge_title_set_target_day,
+                descriptionRes = R.string.feature_addchallenge_description_set_target_day,
             )
             Spacer(modifier = Modifier.height(100.dp))
-            TargetDaysSelector(
+            TargetDay(
                 targetDays = uiState.targetDays,
-                minDays = MIN_CHALLENGE_TARGET_DAYS,
-                maxDays = MAX_CHALLENGE_TARGET_DAYS,
                 onTargetDaysUpdated = { processAction(AddChallengeUiAction.OnTargetDaysUpdated(it)) },
-                containerColor = CustomColorProvider.colorScheme.surface,
-                contentColor = CustomColorProvider.colorScheme.onSurface,
-                shape = MaterialTheme.shapes.large,
-                textStyle = MaterialTheme.typography.displaySmall,
                 modifier = Modifier.align(Alignment.CenterHorizontally),
             )
-            Spacer(modifier = Modifier.height(20.dp))
-            TargetDaySwitch(
-                leftText = stringResource(id = AddChallengeStrings.feature_addchallenge_target_day),
-                rightText = stringResource(id = AddChallengeStrings.feature_addchallenge_unlimited),
-                isLeftSelected = uiState.targetDays is TargetDays.Fixed,
-                onSwitchChange = { isLeftSelected ->
-                    processAction(
-                        AddChallengeUiAction.OnTargetDaysUpdated(
-                            if (isLeftSelected) {
-                                TargetDays.Fixed(INIT_CHALLENGE_TARGET_DAYS)
-                            } else {
-                                TargetDays.Infinite
-                            },
-                        ),
-                    )
-                },
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-            )
-        }
-    }
-}
-
-@Composable
-private fun TargetDaysSelector(
-    targetDays: TargetDays,
-    minDays: Int,
-    maxDays: Int,
-    onTargetDaysUpdated: (TargetDays) -> Unit,
-    containerColor: Color,
-    contentColor: Color,
-    shape: Shape,
-    textStyle: TextStyle,
-    modifier: Modifier = Modifier,
-    width: Dp = 140.dp,
-    height: Dp = 80.dp,
-) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        when (targetDays) {
-            is TargetDays.Fixed -> {
-                CursorLessNumberTextField(
-                    value = targetDays.days,
-                    onValueChange = { onTargetDaysUpdated(TargetDays.Fixed(it)) },
-                    minLimit = minDays,
-                    maxLimit = maxDays,
-                    shape = shape,
-                    textBackground = containerColor,
-                    textColor = contentColor,
-                    textStyle = textStyle,
-                    modifier = Modifier.size(width = width, height = height),
-                )
-            }
-
-            TargetDays.Infinite -> {
-                Box(
-                    modifier = Modifier.size(width = width, height = height),
-                    contentAlignment = Alignment.Center,
-                ) {
-                    Icon(
-                        painter = painterResource(id = ChallengeTogetherIcons.Infinite),
-                        tint = CustomColorProvider.colorScheme.onBackground,
-                        contentDescription = stringResource(
-                            id = AddChallengeStrings.feature_addchallenge_unlimited,
-                        ),
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(16.dp),
-                    )
-                }
-            }
-        }
-        Spacer(modifier = Modifier.width(16.dp))
-        Text(
-            text = stringResource(id = AddChallengeStrings.feature_addchallenge_days),
-            color = CustomColorProvider.colorScheme.onBackground,
-            style = MaterialTheme.typography.displaySmall,
-        )
-    }
-}
-
-@Composable
-private fun TargetDaySwitch(
-    leftText: String,
-    rightText: String,
-    isLeftSelected: Boolean,
-    onSwitchChange: (Boolean) -> Unit,
-    modifier: Modifier = Modifier,
-    switchBackgroundColor: Color = CustomColorProvider.colorScheme.surface,
-    selectedColor: Color = CustomColorProvider.colorScheme.brand,
-    onSelectedColor: Color = CustomColorProvider.colorScheme.onBrand,
-    unselectedColor: Color = CustomColorProvider.colorScheme.onSurface.copy(alpha = 0.3f),
-    textStyle: TextStyle = MaterialTheme.typography.labelSmall,
-    shape: Shape = MaterialTheme.shapes.extraLarge,
-    gap: Dp = 6.dp,
-) {
-    val density = LocalDensity.current
-
-    var leftWidth by remember { mutableIntStateOf(0) }
-    var rightWidth by remember { mutableIntStateOf(0) }
-
-    val offsetX by animateDpAsState(
-        targetValue = if (isLeftSelected) 0.dp else leftWidth.dp,
-        label = "SwitchOffset Animation",
-    )
-
-    val animatedWidth by animateDpAsState(
-        targetValue = if (isLeftSelected) leftWidth.dp else rightWidth.dp,
-        label = "SwitchWidth Animation",
-    )
-
-    Box(
-        modifier = modifier
-            .height(IntrinsicSize.Min)
-            .clip(shape)
-            .background(switchBackgroundColor)
-            .padding(gap),
-    ) {
-        Box(
-            modifier = Modifier
-                .offset { IntOffset(offsetX.roundToPx(), 0) }
-                .width(animatedWidth)
-                .fillMaxHeight()
-                .clip(shape)
-                .background(selectedColor),
-        )
-
-        Row(
-            modifier = Modifier.fillMaxHeight(),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Box(
-                modifier = Modifier
-                    .clickable(
-                        interactionSource = remember { MutableInteractionSource() },
-                        indication = null,
-                    ) { onSwitchChange(true) }
-                    .onGloballyPositioned { coordinates ->
-                        leftWidth = with(density) { coordinates.size.width.toDp().value.toInt() }
-                    }
-                    .padding(vertical = 8.dp, horizontal = 16.dp),
-                contentAlignment = Alignment.Center,
-            ) {
-                Text(
-                    text = leftText,
-                    textAlign = TextAlign.Center,
-                    color = if (isLeftSelected) onSelectedColor else unselectedColor,
-                    style = textStyle,
-                )
-            }
-            Box(
-                modifier = Modifier
-                    .clickable(
-                        interactionSource = remember { MutableInteractionSource() },
-                        indication = null,
-                    ) { onSwitchChange(false) }
-                    .onGloballyPositioned { coordinates ->
-                        rightWidth = with(density) { coordinates.size.width.toDp().value.toInt() }
-                    }
-                    .padding(vertical = 8.dp, horizontal = 16.dp),
-                contentAlignment = Alignment.Center,
-            ) {
-                Text(
-                    text = rightText,
-                    textAlign = TextAlign.Center,
-                    color = if (isLeftSelected) unselectedColor else onSelectedColor,
-                    style = textStyle,
-                )
-            }
         }
     }
 }
