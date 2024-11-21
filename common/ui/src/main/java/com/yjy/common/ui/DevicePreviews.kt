1d10b142d436c0c6301aca3a5ae82f0aad2effc8
junyong <junyong008@gmail.com>
2024-11-21 20:14:15 +0900

    refactor: DevicePreviews 문법 수정

--- common/ui/src/main/java/com/yjy/common/ui/DevicePreviews.kt
+++ common/ui/src/main/java/com/yjy/common/ui/DevicePreviews.kt
@@ -2,7 +2,7 @@ package com.yjy.common.ui
 
 import androidx.compose.ui.tooling.preview.Preview
 
-@Preview(name = "phone", device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480")
-@Preview(name = "landscape", device = "spec:shape=Normal,width=640,height=360,unit=dp,dpi=480")
-@Preview(name = "foldable", device = "spec:shape=Normal,width=673,height=841,unit=dp,dpi=480")
+@Preview(name = "phone", device = "spec:width=360dp,height=640dp,dpi=480")
+@Preview(name = "landscape", device = "spec:width=640dp,height=360dp,dpi=480")
+@Preview(name = "foldable", device = "spec:width=673dp,height=841dp,dpi=480")
 annotation class DevicePreviews
