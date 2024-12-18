9c845cd2638adf67395430d9398a2b06a35412f6
junyong <junyong008@gmail.com>
2024-12-19 00:10:39 +0900

    feat: FcmEntryPoint에 AuthRepository 의존성 주입 추가

--- app/src/main/java/com/yjy/challengetogether/di/FirebaseModule.kt
+++ app/src/main/java/com/yjy/challengetogether/di/FirebaseModule.kt
@@ -3,6 +3,7 @@ package com.yjy.challengetogether.di
 import com.yjy.challengetogether.fcm.DefaultFcmTokenProvider
 import com.yjy.challengetogether.fcm.NotificationMapper
 import com.yjy.data.auth.api.AppLockRepository
+import com.yjy.data.auth.api.AuthRepository
 import com.yjy.data.user.api.FcmTokenProvider
 import com.yjy.data.user.api.NotificationRepository
 import dagger.Binds
@@ -24,6 +25,7 @@ internal abstract class FirebaseModule {
 @InstallIn(SingletonComponent::class)
 @EntryPoint
 interface FcmEntryPoint {
+    fun authRepository(): AuthRepository
     fun notificationRepository(): NotificationRepository
     fun appLockRepository(): AppLockRepository
     fun notificationMapper(): NotificationMapper
