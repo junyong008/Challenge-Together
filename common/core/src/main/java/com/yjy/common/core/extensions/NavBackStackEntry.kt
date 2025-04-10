c17c74bb9e73149ebd8ccf0b4a0c5616994cb853
junyong <junyong008@gmail.com>
2025-04-11 08:52:14 +0900

    Fix/crash (#19)
    * fix: 크래시 가능성 요소 보완
    
    - 애니메이션 Nan 값 방지
    - 위젯 ID 타이밍 문제 방지
    - ViewModel 공유 타이밍 문제 방지
    
    * chore: 코드 스타일 준수

--- common/core/src/main/java/com/yjy/common/core/extensions/NavBackStackEntry.kt
+++ common/core/src/main/java/com/yjy/common/core/extensions/NavBackStackEntry.kt
@@ -6,14 +6,30 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
+import timber.log.Timber
 
 @Composable
 inline fun <reified T : ViewModel> NavBackStackEntry.sharedViewModel(
     navController: NavHostController,
 ): T {
-    val navGraphRoute = destination.parent?.route ?: return hiltViewModel()
-    val parentEntry = remember(this) {
-        navController.getBackStackEntry(navGraphRoute)
+    val parentRoute = destination.parent?.route
+    val parentEntry = remember(this, parentRoute) { safeGetBackStackEntry(navController, parentRoute) }
+    return if (parentEntry != null) {
+        hiltViewModel(parentEntry)
+    } else {
+        hiltViewModel()
     }
-    return hiltViewModel(parentEntry)
+}
+
+fun safeGetBackStackEntry(
+    navController: NavHostController,
+    route: String?,
+): NavBackStackEntry? {
+    if (route == null) return null
+    return try {
+        navController.getBackStackEntry(route)
+    } catch (e: IllegalArgumentException) {
+        Timber.e(e, "BackStackEntry not found for route: $route")
+        null
+    }
 }
